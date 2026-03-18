ffc713a6d854128b26518cb502c4a45b46c73f2c
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-18 15:19:59 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Routes from './routes';
+  
 
 function App() {
   return (
     <div className="">
-      <h1>Projeto</h1>     
+      <Routes />   
     </div>
   );
 }
