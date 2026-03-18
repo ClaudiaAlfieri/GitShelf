c47fb31b447accd9658d3a02ed8c8dbdc8787845
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-18 16:06:46 +0000

    Update Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
-
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
-export default function Main() {
-  return (
-    <div className="">
-      <h1>Repositorio</h1>     
-    </div>
-  );
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main(){
+  return(
+    <Container>      
+      <h1>
+        <FaGithub size={25}/>
+        Meus Repositórios
+      </h1>
+
+      <Form>
+        <input type="text" placeholder="Adicionar Repositórios"/>
+
+        <SubmitButton>
+          <FaPlus size={14} color="#fff"/>
+
+        </SubmitButton>
+
+      </Form>
+    </Container>
+  )
 }
