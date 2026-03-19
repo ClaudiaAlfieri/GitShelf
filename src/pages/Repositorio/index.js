c7d1b32183afb8cafb0fb0b959f398a081ce5e99
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-19 15:36:57 +0000

    Update Repositorio page

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,11 +1,7 @@
-
-
 import React from 'react';
 
-export default function Repositorio() {
-  return (
-    <div className="">
-      <h1>Projeto</h1>     
-    </div>
-  );
+export default function Repositorio(){
+  return(
+    <h1>Repositorio</h1>
+  )
 }
