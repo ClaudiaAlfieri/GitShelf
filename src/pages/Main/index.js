78deecb36901a1608af7d24f4481ba55cca7c923
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-23 14:34:58 +0000

    Update Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton, List, DeleteButton  } from './styles';
@@ -10,15 +10,42 @@ export default function Main(){
   const [ newRepo, setNewRepo ] = useState(''); 
   const [ repositorios, setRepositorios ] = useState([]); 
   const [ loading, setLoading ] = useState(false);
+  const [ alert, setAlert] = useState(null);
+
+  //Buscar
+  useEffect(() => {
+    const repoStorage = localStorage.getItem('repos');
+
+    if(repoStorage){
+      setRepositorios(JSON.parse(repoStorage));
+    }   
+  }, []);
+
+  //Salvar Alterações
+  useEffect(() => {
+    localStorage.setItem('repos', JSON.stringify(repositorios));
+  }, [repositorios]);
 
 const handleSubmit = useCallback ((e)=>{
        e.preventDefault();
 
   async function submit(){
     setLoading(true);
+    setAlert(null);
 
     try{
+
+      if(newRepo === ''){
+        throw new Error('Você precisa indicar um repositório');
+      }
+
       const response = await api.get(`repos/${newRepo}`);
+
+      const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+      if(hasRepo){
+        throw new Error('Repositório duplicado');
+      }
 
     const data = {
       name: response.data.full_name,
@@ -27,6 +54,7 @@ const handleSubmit = useCallback ((e)=>{
     setRepositorios([...repositorios, data]);
     setNewRepo('');
     }catch(error){
+      setAlert(true);
       console.log(error);   
 
   }finally{
@@ -41,6 +69,7 @@ const handleSubmit = useCallback ((e)=>{
 
   function handleinputChange(e){
     setNewRepo(e.target.value);
+    setAlert(null);
   }
 
   const handleDelete = useCallback((repo) =>{
@@ -55,7 +84,7 @@ const handleSubmit = useCallback ((e)=>{
         Meus Repositórios
       </h1>
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error={alert}>
         <input
          type="text" 
          placeholder="Adicionar Repositórios"
