35d2e7e66d987064c93efbaa691ba4031c8f0bba
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-23 14:05:18 +0000

    Create DeleteButton

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 
-import { Container, Form, SubmitButton, List,  } from './styles';
+import { Container, Form, SubmitButton, List, DeleteButton  } from './styles';
 
 import api from '../../services/api';
 
@@ -72,7 +72,9 @@ const handleSubmit = useCallback ((e)=>{
         {repositorios.map(repo =>(
           <li key={repo.name}>
             <span>
-              
+              <DeleteButton onClick={()=>{}}>
+                <FaTrash size={14}/>
+              </DeleteButton>
               {repo.name}
             </span>
             <a href="">
