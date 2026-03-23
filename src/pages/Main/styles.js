f3a8e7862264483f4e2b03fc49628cea21d5f358
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-23 15:04:23 +0000

    Update Repositorio page

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled, { keyframes, css } from 'styled-components';
+import styled, {keyframes, css} from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -17,7 +17,9 @@ export const Container = styled.div`
     svg{
       margin-right: 10px;
     }
+
   }
+
 
 `;
 
@@ -28,7 +30,7 @@ export const Form = styled.form`
 
   input{
     flex:1;
-    border: 1px solid ${props => (props.error ? '#ff0000' : '#eee')};
+    border: 1px solid ${props => (props.error ? '#FF0000' : '#eee')};
     padding: 10px 15px;
     border-radius: 4px;
     font-size: 17px;
@@ -36,20 +38,20 @@ export const Form = styled.form`
 
 `;
 
-//Criando animação do botão
+//Criando animcação do botao
 const animate = keyframes`
   from{
     transform: rotate(0deg);
   }
+
   to{
     transform: rotate(360deg);
   }
 `;
 
-
-export const SubmitButton = styled.button.attrs(props =>({
-    type: 'submit',
-    disabled: props.loading,
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading,
 }))`
   background:#0D2636;
   border: 0;
@@ -61,49 +63,54 @@ export const SubmitButton = styled.button.attrs(props =>({
   align-items: center;
 
 
-    &[disabled]{
-      opacity: 0.5;
-      cursor: not-allowed;
-    }
+  &[disabled]{
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
 
-    ${props => props.loading && css`
+
+  ${props => props.loading &&
+    css`
       svg{
         animation: ${animate} 2s linear infinite;
       }
-    `}
-
-    `;
-
-
-export const List = styled.ul`
-list-style: none;
-margin-top: 20px;
-
-li{
-  padding: 15px 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  
-  & + li{
-    border-top: 1px solid #eee;
+    `
   }
 
-  a{
-    color: #0D2636;
-    text-decoration: none;
-  }  
-}
+
 `;
 
+export const List = styled.ul`
+  list-style:none;
+  margin-top: 20px;
+
+  li{
+    padding: 15px 0;
+    display: flex;
+    flex-direction:row;
+    align-items: center;
+    justify-content: space-between;
+
+    & + li{
+      border-top: 1px solid #eee;
+    }
+
+    a{
+      color:#0D2636;
+      text-decoration: none;
+    }
+  }
+
+`;
+
+
 export const DeleteButton = styled.button.attrs({
-  type: 'button'
+  type:'button'
 })`
-background: transparent;
-color: #0d2636;
-border: 0;
-padding: 8px 7px;
-outline: 0;
-border-radius: 4px;
+  background: transparent;
+  color:#0D2636;
+  border:0;
+  padding: 8px 7px;
+  outline:0;
+  border-radius: 4px;
 `;
